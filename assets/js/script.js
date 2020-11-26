b4a7266c6327a425285ad37f404b8cbad8955f75
dcharney <delaneycharney@gmail.com>
2020-11-26 14:01:37 -0500

    results section displaying properly with overall layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,83 @@
-//user selection feature test
+var liquorSearchEl = $("#liquor-search");
+var proteinSearchEl = $("#protein-search");
+var circleIndicatorOneEl = $("#circle-indicator-1");
+var circleIndicatorTwoEl = $("#circle-indicator-2");
+var drinks = [];
+
+
+// push data from api to drinks array
+var suggestLiquor = function(data) {
+    data.drinks.forEach(element => {
+        drinks.push(element.strIngredient1);
+    });
+}
+
+var fetchLiquorIngredientList = function() {
+    fetch("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list")
+    .then(function(response) {
+        if(response.ok) {
+            response.json().then(function(data) {
+                suggestLiquor(data); // populate suggestions
+            });
+        } else {
+            console.log("no good baby");
+        }
+    });
+}
+
+var fetchLiquorSearch = function(keyword) {
+    fetch("https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + keyword)
+        .then(function(response) {
+            if(response.ok) {
+                response.json().then(function(data) {
+                    console.log(data);
+                });
+            } else {
+                console.log("no good baby");
+            }
+        });
+}
+var submitHandler = function(event) {
+    // prevent from refreshing page
+    event.preventDefault()
+
+    // switch to second input field if on first one
+    if(circleIndicatorOneEl.hasClass("active") ) {
+        // removing visibility from first input and cirle indicator
+        proteinSearchEl.removeClass("active");
+        circleIndicatorOneEl.removeClass("active");
+
+        // adding visibility to second input and circle indicator and focus on input
+        liquorSearchEl.addClass("active");
+        circleIndicatorTwoEl.addClass("active");
+        liquorSearchEl.focus();
+
+    } else { // user is on second field(liquor input)
+        // check if user added protein and liquor before sending data
+        if(!proteinSearchEl.val() || proteinSearchEl.val() == " ") {
+            console.log("no protein entered");
+        } else {
+            // fetch food function here
+        }
+
+        if(!liquorSearchEl.val() || liquorSearchEl.val() == " ") { // if not valid or just empty spaces
+            console.log("no liquor entered");
+        } else {
+            fetchLiquorSearch(liquorSearchEl.val());
+        }
+        // scroll to results section after search
+        scrollTo(0, $('#results-section').offset().top);
+    }
+
+}
+
+// submit listener
+$("#search-form").on("submit", submitHandler);
+
+// auto complete liquor search
+fetchLiquorIngredientList();
+liquorSearchEl.autocomplete({ 
+    source: drinks,
+    appendTo: "#suggestions-wrapper",
+    autoFocus: true
+});
