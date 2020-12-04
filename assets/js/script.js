ba6157775568d117b314a49eab02fdab66ac2991
Ronan <galv.ronan@gmail.com>
2020-12-03 21:05:24 -0500

    merge food-choice

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,13 +16,23 @@ var enableScroll = function() {
     mainEl.css("overflow-y","scroll");
 }
 var drinksSavedEl = $("#drinks-saved");
+var meals = [];
+var mealResultsEl = $("#meal-results");
 var drinks = [];
 
 //recipe book save data
 var drinkRecipeBookData = [];
 
 
-// push data from api to drinks array
+// push data from api to food array
+var suggestMeal = function(data) {
+
+    data.meals.forEach(element => {
+        meals.push(element.strIngredient);
+    });
+}
+
+//push data from api to drink array
 var suggestLiquor = function(data) {
     data.drinks.forEach(element => {
         drinks.push(element.strIngredient1);
@@ -120,6 +130,20 @@ var openDrinkModal = function(data) {
 }
 
 // fetch list of ingrediants/liquors to add to suggestion
+var fetchMealIngredientList = function() {
+    fetch("https://www.themealdb.com/api/json/v1/1/list.php?i=list")
+    .then(function(response) {
+        if(response.ok) {
+            response.json().then(function(data) {
+
+                suggestMeal(data); // populate suggestions
+            });
+        } else {
+            console.log("not feeling it...");
+        }
+    });
+}
+
 var fetchLiquorIngredientList = function() {
     fetch("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list")
     .then(function(response) {
@@ -134,6 +158,19 @@ var fetchLiquorIngredientList = function() {
 }
 
 // fetch the drinks for the liquor that the user searched
+var fetchMealSearch = function(keyword) {
+    fetch("https://www.themealdb.com/api/json/v1/1/filter.php?i=" + keyword)
+        .then(function(response) {
+            if(response.ok) {
+                response.json().then(function(data) {
+                    mealResults(data);
+                });
+            } else {
+                console.log("not feeling it...");
+            }
+        });
+}
+
 var fetchLiquorSearch = function(keyword) {
     fetch("https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + keyword)
         .then(function(response) {
@@ -169,7 +206,7 @@ var submitHandler = function(event) {
 
     // switch to second input field if on first one
     if(circleIndicatorOneEl.hasClass("active") ) {
-        // removing visibility from first input and cirle indicator
+        // removing visibility from first input and circle indicator
         proteinSearchEl.removeClass("active");
         circleIndicatorOneEl.removeClass("active");
 
@@ -181,9 +218,10 @@ var submitHandler = function(event) {
     } else { // user is on second field(liquor input)
         // check if user added protein and liquor before sending data
         if(!proteinSearchEl.val() || proteinSearchEl.val() == " ") {
-            console.log("no protein entered");
+            console.log("no meal ingredient entered");
         } else {
             // fetch food function here
+            fetchMealSearch(proteinSearchEl.val());
         }
 
         if(!liquorSearchEl.val() || liquorSearchEl.val() == " ") { // if not valid or just empty spaces
@@ -315,8 +353,103 @@ var loadDrinkRecipeBook = function() {
 }
 // load / populate drink section of recipe book when page loads
 loadDrinkRecipeBook();
+//push meal array into results container
+var mealResults = function(data) {
+        data.meals.forEach(element => {
+        //wrapper for ind. meals
+        var singleMealDiv = document.createElement("div");
+        //bg img
+        var mealImg = document.createElement("img");
+        //wrapper for title
+        var mealHeader = document.createElement("div");
+        // title
+        var mealTitle = document.createElement("h6");
+
+        // add classes needed to elements
+        $(singleMealDiv).addClass("cell large-4 result-cell");
+        $(mealHeader).addClass("result-bg");
+        $(mealTitle).addClass("result");
+
+
+        // add data-img of meal
+        $(mealImg).attr("src", element.strMealThumb);
+        // add text data-title of meal
+        $(mealTitle).text(element.strMeal);
+        // set meal id to cell id
+        $(singleMealDiv).attr("id", element.idMeal);
+
+
+        // put h4 in h4 wrapper
+        mealHeader.append(mealTitle);
+        // put h4 and img in cell
+        singleMealDiv.append(mealImg, mealHeader);
+        // append cell to results element
+        mealResultsEl.append(singleMealDiv);
+
+        //could we use a modal once clicking on a recipe to display ingredients or recipe and give close button option and recipe book option?
+    });
+}
+
+// fetch clicked meal from results
+var fetchMealInfo = function(idMeal) {
+    // search by id
+    fetch("https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + idMeal)
+        .then(function(response) {
+            console.log(response);
+            if(response.ok) {
+                response.json().then(function(data) {
+                    openMealModal(data);
+                });
+            } else {
+                console.log("no good baby");
+            }
+        });
+}
+
+var openMealModal = function(data) {
+    var dataInfo = data.meals[0];
+    var mealName = dataInfo.strMeal;
+    var instructions = dataInfo.strInstructions;
+    var img = dataInfo.strMealThumb;
+    var ingredients = [];
+
+    // Filtering through the objects to find ingredients and put their values in an array
+    for(var i = 0; i < 15; i++) { // ingredients list goes to 15 max in api
+        if(dataInfo["strIngredient"+i]){
+            ingredients.push(dataInfo["strIngredient"+i]);
+        };
+    }
+    console.log(
+        mealName,
+        instructions,
+        img,
+        ingredients
+    );
+
+    var modal = new Foundation.Reveal($('#meal-modal'));
+    modal.open();
+}
+
+var mealClicked = function(event) {
+     // find meals container
+    var meal = $(event.target).closest(".result-cell");
+    // get id (unique meal id needed for api search)
+    var idMeal = $(meal).attr("id");
+
+    fetchMealInfo(idMeal);
+}
+
+
 // submit listener
 $("#search-form").on("submit", submitHandler);
+
+// auto complete meal search
+fetchMealIngredientList();
+proteinSearchEl.autocomplete({
+    source: meals,
+    appendTo: "#suggestions-wrapper",
+    autoFocus: true
+});
 
 // auto complete liquor search
 fetchLiquorIngredientList();
@@ -331,3 +464,5 @@ $(drinkResultsEl).on("click", drinkClicked);
 $(drinksSavedEl).on("click", drinkClicked);
 // save to recipe btn listener
 $("#save-btn").on("click", saveDrinkRecipe);
+// meal selected listener
+$(mealResultsEl).on("click", mealClicked);
