0fe74a0e366d2466c41136f6e80f6f25f209d11c
unknown <jared.davis21@yahoo.com>
2020-12-06 17:01:31 -0500

    refined modal and updated html and css

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,12 +4,15 @@ var circleIndicatorOneEl = $("#circle-indicator-1");
 var circleIndicatorTwoEl = $("#circle-indicator-2");
 var meals = [];
 var mealResultsEl = $("#meal-results");
+var mealsSavedEl = $("#meals-saved");
 var drinks = [];
+
+//recipe book data
+var mealRecipeBookData = [];
 
 
 // push data from api to food array
 var suggestMeal = function(data) {
-    
     data.meals.forEach(element => {
         meals.push(element.strIngredient);
     });
@@ -143,7 +146,7 @@ var mealResults = function(data) {
         // append cell to results element
         mealResultsEl.append(singleMealDiv);
 
-        //could we use a modal once clicking on a recipe to display ingredients or recipe and give close button option and recipe book option?
+    
     });
 }
 
@@ -164,16 +167,19 @@ var fetchMealInfo = function(idMeal) {
 }
 
 var openMealModal = function(data) {
+    
     var dataInfo = data.meals[0];
+    var mealId = dataInfo.idMeal;
     var mealName = dataInfo.strMeal;
     var instructions = dataInfo.strInstructions;
     var img = dataInfo.strMealThumb;
     var ingredients = [];
+    var saveText = "";
 
     // Filtering through the objects to find ingredients and put their values in an array
     for(var i = 0; i < 15; i++) { // ingredients list goes to 15 max in api
         if(dataInfo["strIngredient"+i]){
-            ingredients.push(dataInfo["strIngredient"+i]);
+            ingredients.push(" " + dataInfo["strIngredient"+i]);
         };
     }
     console.log(
@@ -183,10 +189,36 @@ var openMealModal = function(data) {
         ingredients
     );
 
+    // Find if the array already has recipe as object by comparing the property value then
+    // if in recipe book, update to text to "remove from recipe book" else to "save to recipe"
+    if(mealRecipeBookData.some(recipe => recipe.mealName === mealName)){
+        saveText = "Remove From Recipe Book";
+    } else {
+        saveText = "Save To Recipe Book";
+    }
+
+    // update text and add meal id date in save btn
+    $("#save-btn")
+        .text(saveText)
+        .attr('data-mealId', mealId);
+
+    // update meal name in modal
+    $("#meal-name").text(mealName);
+    // update meal details: add category, alchohol, glass, instructions, ingredients
+    $("#meal-highlight-details").html(
+        "<p>Instructions: "+ instructions + "</p>" +
+        "<br>" +
+        "<p id='meal-ingredients'>Ingredients: "+ ingredients + "</p>"
+    );
+    
+    //update img
+    $("#meal-highlight-img").html("<img src='" +img+"'/>");
+    
+    //open food modal
     var modal = new Foundation.Reveal($('#meal-modal'));
     modal.open();
 }
-
+    
 var mealClicked = function(event) {
      // find meals container
     var meal = $(event.target).closest(".result-cell");
@@ -195,7 +227,117 @@ var mealClicked = function(event) {
 
     fetchMealInfo(idMeal);
 }
- 
+
+//placeholder function for shopping list
+var addMealShoppingList = function() {
+    // find ingredients
+    var ingredients = $("#meal-ingredients").text();
+
+    console.log(ingredients);
+}
+
+var populateMealRecipeBook = function(name, image, id, addRecipe) {
+    // if adding to recipe book
+    if(addRecipe === true) {
+        // cell wrapper for individual drinks
+        var cellDiv = document.createElement("div");
+        // bg img in wrapper
+        var img = document.createElement("img");
+        // div wrapper for title/drink name
+        var h6Div = document.createElement("div");
+        // title/ drink name
+        var h6 = document.createElement("h6");
+
+        // add classes needed to elements
+        $(cellDiv).addClass("cell large-4 result-cell");
+        $(h6Div).addClass("result-bg");
+        $(h6).addClass("result");
+    
+        // add data-img
+        $(img).attr("src", image);
+        // add text data-title of drink
+        $(h6).text(name);
+        // set drink id to cell id
+        $(cellDiv).attr("id", id);
+    
+        // put h4 in h4 wrapper
+        h6Div.append(h6);
+        // put h4 and img in cell
+        cellDiv.append(img, h6Div);
+        // append cell to results element
+        $("#meals-saved").append(cellDiv);
+    }
+    // else if removing from recipe book
+    else { 
+        // remove from saved recipe book section only
+        $(mealsSavedEl).find("#"+id).remove();
+    }
+}
+
+// save & remove recipe function
+var saveMealRecipe = function(event) {
+    // grab meal name
+    var name = $(event.target).closest("#meal-modal").find("#meal-name").text();
+    // grab image
+    var image = $(event.target).closest("#meal-modal").find("img").attr('src');
+    // grab meal unique id
+    var id = $(event.target).attr("data-mealId");
+    // add or remove var
+    var addRecipe = true;
+
+    // save to recipe book / local storage
+    var recipeData = {mealName: name, mealImg: image, mealId: id};
+
+    // Find if the array already has recipe as object by comparing the property value
+    if(mealRecipeBookData.some(recipe => recipe.mealName === name)){
+        // delete if already saved (clicked "remove from recipe book)") by
+        // updating recipe book var to not include this recipe
+        mealRecipeBookData = mealRecipeBookData.filter(recipe => recipe.mealName !== name);
+        // update local storage to array
+        localStorage.setItem('mealRecipeBookData', JSON.stringify(mealRecipeBookData));
+        // remove from recipe book
+        addRecipe = false;
+        // update text of modal
+        $("#save-btn").text("Save To Recipe Book");
+
+    } else { 
+        // not a duplicate so save (clicked "save to recipe book")
+        mealRecipeBookData.push(recipeData);
+        // update local storage to array
+        localStorage.setItem('mealRecipeBookData', JSON.stringify(mealRecipeBookData));
+        // add to recipe book
+        addRecipe = true;
+        // update text of modal
+        $("#save-btn").text("Remove From Recipe Book");
+
+    }
+
+    // populate to recipe book right away 
+    populateMealRecipeBook(name, image, id, addRecipe);
+
+}
+
+// load meal local storage data
+var loadMealRecipeBook = function() {
+    var localData = JSON.parse(localStorage.getItem("mealRecipeBookData"));
+    // check if empty before loading
+    if (localData) {
+        // update array to local storage
+        mealRecipeBookData = localData;
+        // populate recipe book section with saved data
+        mealRecipeBookData.forEach(element => {
+            populateMealRecipeBook(element.mealName, element.mealImg, element.mealId, true);
+        });
+    } else {
+        return
+    }
+
+}
+// load / populate meal section of recipe book when page loads
+loadMealRecipeBook();
+// submit listener
+$("#search-form").on("submit", submitHandler);
+
 
 // submit listener
 $("#search-form").on("submit", submitHandler);
@@ -218,3 +360,6 @@ liquorSearchEl.autocomplete({
 
 // meal selected listener
 $(mealResultsEl).on("click", mealClicked);
+$(mealsSavedEl).on("click", mealClicked);
+// save to recipe btn listener
+$("#save-btn").on("click", saveMealRecipe);
