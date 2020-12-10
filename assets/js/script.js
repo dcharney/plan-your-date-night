431a4318f26cae60800eba79ae44b781ae4e29b3
dcharney <delaneycharney@gmail.com>
2020-12-10 13:47:37 -0800

    search section complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,8 @@
-// gather main element
+// gather elements
 var mainEl = $("main");
-
-// gather html search section elements
-var proteinSearchEl = $("#protein-search");
-var liquorSearchEl = $("#liquor-search");
+var searchEl = $("#search-input");
+var circleIndicatorOneEl = $("#circle-indicator-1");
+var circleIndicatorTwoEl = $("#circle-indicator-2");
 
 // build arrays to store local data
 suggestions = [];
@@ -12,12 +11,14 @@ searchTools = {
     meals:{
         listURL:"https://www.themealdb.com/api/json/v1/1/list.php?i=list",
         targetContainer:"meals",
-        targetElement:"strIngredient"
+        targetElement:"strIngredient",
+        searchTerm:""
     }, 
     drinks:{
         listURL:"https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list",
         targetContainer:"drinks",
-        targetElement:"strIngredient1"
+        targetElement:"strIngredient1",
+        searchTerm:""
     }
 };
 
@@ -46,29 +47,65 @@ var prepAutocomplete = function(searchArr) {
                 console.log("invalid api url");
             }
         });
+    
+    // assign suggestions to element
+    searchEl.autocomplete({
+        source: suggestions,
+        appendTo: "#suggestions-wrapper",
+        autoFocus: true
+    });
 }
 // END AUTOCOMPLETE //
 
-// event handler when search form is submitted
+// START SEARCH FUNCTIONS
+// event handler for search form
 var submitHandler = function(event) {
     // prevent from refreshing page
     event.preventDefault();
-}
 
+    // check if current submission is valid
+    if (!searchEl.val() || searchEl.val() == " ") {
+        console.log("please enter a valid search term");
+        return;
+    }
+
+    // if meal is being submitted, transition to drink search
+    if (circleIndicatorOneEl.hasClass("active")) {
+        // save current search term
+        searchTools.meals.searchTerm = searchEl.val();
+
+        // clear input and update placeholder
+        searchEl.val("");
+        searchEl.attr("placeholder","Search Liquor");
+
+        // update suggestions array
+        prepAutocomplete(searchTools.drinks);
+
+        // update circle indication
+        circleIndicatorOneEl.removeClass("active");
+        circleIndicatorTwoEl.addClass("active");
+        
+    } else { 
+        // save current search term
+        searchTools.drinks.searchTerm = searchEl.val();
+        // scroll to results section
+        mainEl[0].scrollTo(0, $('#results-section').offset().top);
+        // populate results
+        //fetchLiquorSearch(liquorSearchEl.val());
+    }
+}
+// END SEARCH FUNCTIONS
+
+
+// START UP FUNCTIONS
 // assign search suggestions (autocomplete) to input elements
 prepAutocomplete(searchTools.meals);
-proteinSearchEl.autocomplete({
-    source: suggestions,
-    appendTo: "#suggestions-wrapper",
-    autoFocus: true
-});
-
-liquorSearchEl.autocomplete({
-    source: drinks,
-    appendTo: "#suggestions-wrapper",
-    autoFocus: true
-});
+// END START UP FUNCTIONS
 
 
+// EVENT HANDLERS START
 // add event handler to search section 
 $("#search-form").on("submit", submitHandler);
+
+// EVENT HANDLERS END
+
