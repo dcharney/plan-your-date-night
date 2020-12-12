eae8dea6db8ce8305a5e8fd99667f23088785865
dcharney <delaneycharney@gmail.com>
2020-12-12 01:43:09 -0800

    modal when result clicked, instructions scrollbar added, link to recipe added

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,24 +11,20 @@ suggestions = [];
 meals = {
     listURL:"https://www.themealdb.com/api/json/v1/1/list.php?i=list",
     indexSearchURL:"https://www.themealdb.com/api/json/v1/1/filter.php?i=",
+    idSearchURL:"https://www.themealdb.com/api/json/v1/1/lookup.php?i=",
     targetContainer:"meals",
     targetElement:"strIngredient",
     searchTerm:"",
-    resultIcon:"strMealThumb",
-    resultTitle:"strMeal",
-    resultIdNo:"idMeal",
-    resultId:"#meal-results"
+    type:"Meal"
 }
 drinks = {
     listURL:"https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list",
     indexSearchURL:"https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=",
+    idSearchURL:"https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=",
     targetContainer:"drinks",
     targetElement:"strIngredient1",
     searchTerm:"",
-    resultIcon:"strDrinkThumb",
-    resultTitle:"strDrink",
-    resultIdNo:"idDrink",
-    resultId:"#drink-results"
+    type:"Drink"
 }
 
 // START AUTOCOMPLETE //
@@ -132,7 +128,7 @@ var submitHandler = function(event) {
 // FUNCTION to gather the search results and build the results elements
 // IN: Data Array containing result type and searched term
 var generateResults = function(type) {
-    // unpack data array
+    // unpack type array
     var targetContainer = type.targetContainer;
     var indexSearchURL = type.indexSearchURL;
     var searchTerm = type.searchTerm;
@@ -156,11 +152,13 @@ var generateResults = function(type) {
 // FUNCTION to build and append a single result cell to a result container
 // IN: element containing data from a single result
 var generateResultElement = function(type, element) {
-    // unpack data array
-    var resultIcon = type.resultIcon;
-    var resultTitle = type.resultTitle;
-    var resultIdNo = type.resultIdNo;
-    var resultEl = $(type.resultId);
+    // unpack type array
+    var resultIcon = "str" + type.type + "Thumb";
+    var resultTitle = "str" + type.type;
+    var resultIdNo = "id" + type.type;
+    var resultContainerId = "#" + type.type.toLowerCase() + "-results";
+    var resultEl = $(resultContainerId);
+
 
     // cell wrapper for individual result
     var cellDiv = document.createElement("div");
@@ -195,11 +193,119 @@ var generateResultElement = function(type, element) {
 
 
 
+// START MODAL FUNCTIONS
+
+// FUNCTION to run whenever a result cell is clicked, generates a modal to display the corresponding api data for the selected cell
+// IN: event object (cell being clicked)
+var resultClicked = function(event) {
+    // get container
+    var container = $(event.target).closest(".result-cell");
+    // get unique ip for api search
+    var resultId = $(container).attr("id");
+
+    // get result type (meal or drink)
+    var type = getResultType(event);
+
+    // fetch api data for modal
+    fetchResultInfo(type, resultId);
+}
+
+// FUNCTION to distinguish between meal and drink results
+// IN: event object (cell being clicked)
+// OUT: type array containing necessary search terms for generating meal or drink modal
+var getResultType = function(event) {
+    // get result container id
+    var resultContainer = $(event.target).closest(".results");
+    var resultContainerId = resultContainer.attr("id");
+    var type = "";
+
+    // assign type based on id
+    if (resultContainerId == "meal-results") {
+        type = meals;
+    } else if (resultContainerId == "drink-results") {
+        type = drinks;
+    }
+    return type
+}
+
+// FUNCTION dafhaerhaerh
+// IN: ASDLKGJLJ
+var fetchResultInfo = function(type, resultId) {
+    // unpack data array
+    var idSearchURL = type.idSearchURL;
+
+    // get api data for clicked result id
+    fetch(idSearchURL + resultId)
+        .then(function(response) {
+            if(response.ok) {
+                response.json().then(function(data) {
+                    // display data in modal
+                    openModal(type, data);
+                })
+            } else {
+                console.log("no good baby");
+            }
+        });
+}
+
+var openModal = function(type, data) {
+    // unpack type array
+    var targetContainer = type.targetContainer;
+    
+    // unpack data array
+    var dataInfo = data[targetContainer][0];
+    var modalId = dataInfo["id" + type.type];
+    var modalTitle = dataInfo["str" + type.type];
+    var modalInstructions = dataInfo.strInstructions;
+    //modalInstructions = modalInstructions.replace('\n', '<<b></b>br>');
+    var modalIcon = dataInfo["str" + type.type + "Thumb"];
+    var modalSource = dataInfo.strSource;
+    var saveText = "";
+
+    // Filtering through the objects to find list of ingredients and put their values in an array
+    var modalIngredients = [];
+    for (var i = 0; i < 15; i++) { // ingredients list goes to 15 max in api
+        if (dataInfo["strIngredient"+i]) {
+            modalIngredients.push(" " + dataInfo["strIngredient"+i]); //added (" " + ) to add spaces after ingredient item
+        };
+    };
+
+    // update modal properties per selected cell
+    $("#result-name").text(modalTitle);
+    
+    // update icon image
+    $("#result-highlight-img").html("<img src='" + modalIcon + "'/>");
+
+    // update source address
+    $("#source").attr("href",modalSource);
+
+    // add descriptive details
+    $("#instructions").text("Instructions: " + modalInstructions);
+    $("#ingredients").text("Ingredients: " + modalIngredients);
+
+    // switch save button text to remove from recipe book if result already saved
+    var saveText = "Save to Recipe Book";
+
+    // add result type and id to save bttn for easier event handling
+    $(".save-btn")
+        .text(saveText)
+        .attr('data-id', modalId)
+        .attr('data-type', type.type);
+
+    // open the modal 
+    var modal = new Foundation.Reveal($('#result-modal'));
+    modal.open();
+}
+
+// END MODAL FUNCTIONS
+
+
+
 // START UP FUNCTIONS
 
 // assign search suggestions (autocomplete) to input elements
 prepAutocomplete(meals);
-
+ 
 // END START UP FUNCTIONS
 
 
@@ -208,6 +314,9 @@ prepAutocomplete(meals);
 
 // add event handler to search section 
 $("#search-form").on("submit", submitHandler);
+
+// run function when a result is selected
+$(".results").on("click", resultClicked);
 
 // EVENT HANDLERS END
 
